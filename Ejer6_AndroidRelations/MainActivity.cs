2fdf698b53858846c14e51da811b56de341b69c1
joaquinBB99 <joaquin.bejarano.brenes@alumnos.fesac.es>
2020-01-15 17:57:24 +0100

    Ejer2

--- Ejer6_AndroidRelations/MainActivity.cs
+++ Ejer6_AndroidRelations/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.Widget;
 
 namespace Ejer6_AndroidRelations
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(Label = "CALCULATOR", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
 
